7e6a94810415273381164df00aea42109c70660c
Marek Zelinka <mzelinka17@gmail.com>
2023-02-14 17:59:15 +0100

    solve ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+
 export function App() {
-  return <div />;
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const addFeedback = (type) => {
+    setFeedback((feedback) => ({
+      ...feedback,
+      [type]: feedback[type] + 1,
+    }));
+  };
+
+  return (
+    <>
+      <h1>Give feedback</h1>
+      <div style={{ display: "flex", gap: 8 }}>
+        <button type="button" onClick={() => addFeedback("good")}>
+          good
+        </button>
+        <button type="button" onClick={() => addFeedback("neutral")}>
+          neutral
+        </button>
+        <button type="button" onClick={() => addFeedback("bad")}>
+          bad
+        </button>
+      </div>
+      <h2>Statistics</h2>
+      <div>
+        good <strong>{feedback.good}</strong>
+      </div>
+      <div>
+        neutral <strong>{feedback.neutral}</strong>
+      </div>
+      <div>
+        bad <strong>{feedback.bad}</strong>
+      </div>
+    </>
+  );
 }
